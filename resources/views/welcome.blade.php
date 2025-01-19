9b32e05af70f29766e071dc5718cc32d0052d448
wen-polo <obeyowen86@gmail.com>
2025-01-19 18:31:22 +0100

    Premier commit - ajout du projet Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resources/css/app.css">
+    <title>Foodbook</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <h1>Bienvenue sur Foodbook !</h1>
+    <script src="resources/js/app.js"></script>
+</body>
+</html>
